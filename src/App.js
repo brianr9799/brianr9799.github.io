cf3e16eaf294fb3f746eb7341a30d7cb38d5e068
brianr9799 <brianr9799@gmail.com>
2023-03-06 20:36:24 -0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/brianr9799"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check Me Out!
         </a>
       </header>
     </div>
